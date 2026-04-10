f31f358e572379f9c4b81cb997e3761bd24eee9a
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-04-10 20:49:37 +0200

    Connect to MongoDB database

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.port;
-if (!PORT) {
+const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+if (!PORT || !DATABASE_URL) {
   throw new Error("Please check .env file");
 }
 export default {
   PORT,
+  DATABASE_URL,
 };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,18 @@
+import mongoose from "mongoose";
 import app from "./app.js";
 import env from "./config/env.js";
 
-const start = () => {
-  app.listen(env.PORT, () => {
-    console.log(`Server running on port: ${env.PORT}`);
-  });
+const start = async () => {
+  try {
+    await mongoose.connect(env.DATABASE_URL);
+    console.log("Conntected to database");
+    app.listen(env.PORT, () => {
+      console.log(`Server running on port: ${env.PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
 
 start();
