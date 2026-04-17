02c40edb1a19de8b9eff52d19c85f689b9a4a335
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-04-17 23:36:05 +0200

    Add AuthForm component

---
+++ blog-app-frontend/src/modules/auth/components/AuthForm.tsx
@@ -0,0 +1,25 @@
+import type { ReactNode, ComponentPropsWithoutRef } from "react";
+import { Link } from "react-router-dom";
+import Button from "../../../shared/Button";
+interface AuthFormProps extends ComponentPropsWithoutRef<"form"> {
+  children: ReactNode;
+  submitButtonText: string;
+  bottomLink: string;
+  bottomLinkText: string;
+}
+export default function AuthForm({
+  children,
+  submitButtonText,
+  bottomLink,
+  bottomLinkText,
+  ...props
+}: AuthFormProps) {
+  return (
+    <form {...props}>
+      {children}
+      <Button type="submit">{submitButtonText}</Button>
+      <Button type="reset">Reset</Button>
+      <Link to={bottomLink}>{bottomLinkText}</Link>
+    </form>
+  );
+}
