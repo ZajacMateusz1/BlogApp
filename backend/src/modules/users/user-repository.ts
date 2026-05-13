20519fae6f3d4f609817cfebd9614a1f806fedaf
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-05-13 20:29:21 +0200

    Add endpoint to get user with latest posts

--- backend/src/modules/users/user-repository.ts
+++ backend/src/modules/users/user-repository.ts
@@ -1,4 +1,15 @@
 import User from "../../models/user-model";
+import Post from "../../models/post-model";
+
 export const getUsersRepository = async () => {
   return await User.find({}, "-password -__v -posts").lean();
+};
+export const getUserRepository = async (userId: string) => {
+  return await User.findById(userId, "-__v -password -posts -email").lean();
+};
+export const getUserPosts = async (userId: string) => {
+  return await Post.find({ creator: userId }, "-__v")
+    .sort({ createdAt: -1 })
+    .limit(10)
+    .lean();
 };
--- backend/src/modules/users/user-routes.ts
+++ backend/src/modules/users/user-routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
-import { getUsers } from "./user-controller";
+import { getUsers, getUser } from "./user-controller";
 
 const router = express.Router();
 router.get("/", getUsers);
+router.get("/:userId", getUser);
+
 export default router;
