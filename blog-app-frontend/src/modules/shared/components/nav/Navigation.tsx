29698521bf3ff5569ae3380e6df9b9608af85879
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-06-05 23:44:16 +0200

    Fix desktop layout bug in RootLayout

--- blog-app-frontend/src/modules/shared/components/nav/Navigation.tsx
+++ blog-app-frontend/src/modules/shared/components/nav/Navigation.tsx
@@ -1,40 +1,53 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Menu, X } from "lucide-react";
 import useAuth from "../../../auth/hooks/useAuth";
 
-interface NavigationProps {
-  openMenu: boolean;
-  handleCloseMenu: () => void;
-}
+import Button from "../Button";
 
-export default function Navigation({
-  openMenu,
-  handleCloseMenu,
-}: NavigationProps) {
+export default function Navigation() {
   const { userId } = useAuth();
   const links = [
     { name: "Main page", link: "/" },
     { name: "Create post", link: "/posts/create" },
     { name: "My profile", link: `/users/${userId}` },
   ];
+
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const handleToggleMenu = () => {
+    setOpenMenu((prev) => !prev);
+  };
+  const handleCloseMenu = () => {
+    setOpenMenu(false);
+  };
+
   return (
-    <nav
-      className={`fixed inset-0 z-10 ${openMenu ? "" : "translate-x-full"} bg-bg-header text-link font-bold transition-transform md:static md:translate-x-0 md:w-full md:max-w-5xl md:mx-auto md:p-4`}
-    >
-      <ul className="h-full w-full flex flex-col justify-center items-center gap-2 md:flex-row md:justify-end md:gap-4">
-        {links.map((link) => (
-          <li key={link.link}>
-            <NavLink
-              onClick={handleCloseMenu}
-              className={({ isActive }) =>
-                `border-b-2 ${isActive ? "text-link-active border-link-active" : "border-transparent"} text-2xl sm:text-3xl font-bold md:text-lg lg:text-xl hover:text-link-hover transition-colors`
-              }
-              to={link.link}
-            >
-              {link.name}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <Button
+        onClick={handleToggleMenu}
+        className="relative z-20 p-3 md:hidden"
+      >
+        {openMenu ? <X /> : <Menu />}
+      </Button>
+      <nav
+        className={`fixed inset-0 z-10 ${openMenu ? "" : "translate-x-full"} bg-bg-header text-link font-bold transition-transform md:static md:translate-x-0 md:w-full md:max-w-5xl md:mx-auto md:p-4`}
+      >
+        <ul className="h-full w-full flex flex-col justify-center items-center gap-2 md:flex-row md:justify-end md:gap-4">
+          {links.map((link) => (
+            <li key={link.link}>
+              <NavLink
+                onClick={handleCloseMenu}
+                className={({ isActive }) =>
+                  `border-b-2 ${isActive ? "text-link-active border-link-active" : "border-transparent"} text-2xl sm:text-3xl font-bold md:text-lg lg:text-xl hover:text-link-hover transition-colors`
+                }
+                to={link.link}
+              >
+                {link.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 }
--- blog-app-frontend/src/modules/shared/layouts/RootLayout.tsx
+++ blog-app-frontend/src/modules/shared/layouts/RootLayout.tsx
@@ -1,26 +1,13 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import { Menu, X } from "lucide-react";
 
 import Navigation from "../components/nav/Navigation";
-import Button from "../components/Button";
 
 export default function RootLayout() {
-  const [openMenu, setOpenMenu] = useState<boolean>(false);
-  const handleToggleMenu = () => {
-    setOpenMenu((prev) => !prev);
-  };
-  const handleCloseMenu = () => {
-    setOpenMenu(false);
-  };
   return (
     <div className="min-h-screen flex flex-col gap-1">
       <header className="sticky z-20 w-full p-1.5 mb-1 md:mb-2 lg:mb-3 flex justify-end bg-bg-header shadow-sm">
-        <Button onClick={handleToggleMenu} className="relative p-3 md:hidden">
-          {openMenu ? <X /> : <Menu />}
-        </Button>
+        <Navigation />
       </header>
-      <Navigation openMenu={openMenu} handleCloseMenu={handleCloseMenu} />
       <main className="flex-1 p-2">
         <Outlet />
       </main>
