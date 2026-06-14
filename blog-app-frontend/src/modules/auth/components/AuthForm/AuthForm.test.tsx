8154c69ba9c813e122bee2894eb63b45465cea81
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-06-14 22:18:40 +0200

    Add AuthForm tests

---
+++ blog-app-frontend/src/modules/auth/components/AuthForm/AuthForm.test.tsx
@@ -0,0 +1,59 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import AuthForm from "./AuthForm";
+import { MemoryRouter } from "react-router-dom";
+
+const defaultProps = {
+  formTitle: "Title",
+  isSubmitting: false,
+  reset: () => {},
+  bottomLink: "/",
+  bottomLinkText: "Link",
+  rootError: null,
+  submitButtonText: "Submit",
+};
+
+describe("AuthForm", () => {
+  it("Renders given props", () => {
+    render(
+      <MemoryRouter>
+        <AuthForm {...defaultProps}>
+          <input type="text" />
+        </AuthForm>
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByRole("heading", { name: "Title" })).toBeInTheDocument();
+    expect(screen.getByRole("link")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+  });
+  it("Reset button works", async () => {
+    const reset = vi.fn();
+    render(
+      <MemoryRouter>
+        <AuthForm {...defaultProps} reset={reset}>
+          <input type="text" />
+        </AuthForm>
+      </MemoryRouter>,
+    );
+    await userEvent.click(screen.getByRole("button", { name: "Reset" }));
+    expect(reset).toHaveBeenCalledOnce();
+  });
+
+  it("Buttons are disabled while submitting", () => {
+    render(
+      <MemoryRouter>
+        <AuthForm {...defaultProps} isSubmitting={true}>
+          <input type="text" />
+        </AuthForm>
+      </MemoryRouter>,
+    );
+
+    const buttons = screen.getAllByRole("button", { name: "Submitting..." });
+    expect(buttons).toHaveLength(2);
+    buttons.forEach((button) => {
+      expect(button).toBeDisabled();
+    });
+  });
+});
