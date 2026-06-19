76e78bb8390dc3cdcd4cd644c5424c8cd90007d6
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-06-19 22:30:02 +0200

    Fix creating query client

--- blog-app-frontend/src/App.tsx
+++ blog-app-frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import { RouterProvider } from "react-router-dom";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import router from "./router/router";
 import AuthContextProvider from "./modules/auth/store/AuthContextProvider/AuthContextProvider";
 import ToastContextProvider from "./modules/shared/store/toast/ToastContextProvider/ToastContextProvider";
+const queryClient = new QueryClient();
 function App() {
-  const queryClient = new QueryClient();
   return (
     <>
       <QueryClientProvider client={queryClient}>
