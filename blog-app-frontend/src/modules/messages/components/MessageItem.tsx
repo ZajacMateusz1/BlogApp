6b8bb36f50a1e3e020ce03dfa348a296cc23c745
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-08-29 19:40:14 +0200

    Add basic version of messages ui

--- blog-app-frontend/src/modules/messages/components/MessageItem.tsx
+++ blog-app-frontend/src/modules/messages/components/MessageItem.tsx
@@ -1,3 +1,14 @@
-export default function MessageItem() {
-  return null;
+import type { MessageDataType } from "../types/messages-types";
+
+interface MessageItemProps {
+  messageData: MessageDataType;
+  isSender: boolean;
+}
+
+export default function MessageItem({
+  messageData,
+  isSender,
+}: MessageItemProps) {
+  console.log(isSender);
+  return <li>{messageData.content}</li>;
 }
--- blog-app-frontend/src/modules/messages/pages/ActiveConversationPage.tsx
+++ blog-app-frontend/src/modules/messages/pages/ActiveConversationPage.tsx
@@ -1,19 +1,22 @@
-import { useState, type SubmitEvent } from "react";
-// import { useInfiniteQuery } from "@tanstack/react-query";
+import { useState, type SubmitEvent, useRef, useEffect } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
-// import useAuth from "../../auth/hooks/useAuth";
+import useAuth from "../../auth/hooks/useAuth";
 import useWebSocket from "../../ws/hooks/useWebSocket";
 import { Send } from "lucide-react";
 
 import type { MessagePayloadType } from "../types/messages-types";
-// import { sendRequest } from "../../../utils/http/http";
-// import type { getMessagesResponseType } from "../types/messages-types";
+import { sendRequest } from "../../../utils/http/http";
+import type { getMessagesResponseType } from "../types/messages-types";
 import Button from "../../shared/components/Button";
 import TextAreaElement from "../../shared/components/form/TextAreaElement";
+import LoadingSpinner from "../../shared/components/LoadingSpinner";
+import ErrorBlock from "../../shared/components/ErrorBlock";
+import MessageItem from "../components/MessageItem";
 
 export default function ActiveConversation() {
-  const { recipientId } = useParams();
-  // const { token } = useAuth();
+  const { conversationId, recipientId } = useParams();
+  const { token, userId } = useAuth();
   const { sendMessage } = useWebSocket();
   const [message, setMessage] = useState<string>("");
   const handleMessageChange = (
@@ -22,29 +25,46 @@ export default function ActiveConversation() {
     setMessage(event.target.value);
   };
 
-  // const {
-  //   data,
-  //   isLoading,
-  //   isError,
-  //   error,
-  //   hasNextPage,
-  //   fetchNextPage,
-  //   isFetchingNextPage,
-  // } = useInfiniteQuery({
-  //   queryKey: ["conversation", conversationId],
-  //   queryFn: ({ pageParam, signal }) =>
-  //     sendRequest<getMessagesResponseType>(
-  //       `/api/messages/${conversationId}/messages?limit=10${pageParam ? `&cursor=${pageParam}` : ""}`,
-  //       {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //         signal,
-  //       },
-  //     ),
-  //   initialPageParam: "",
-  //   getNextPageParam: (lastPage) => lastPage.nextCursor,
-  // });
+  const observerRef = useRef(null);
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    hasPreviousPage,
+    fetchPreviousPage,
+    isFetchingPreviousPage,
+  } = useInfiniteQuery({
+    queryKey: ["conversation", conversationId],
+    queryFn: ({ pageParam, signal }) =>
+      sendRequest<getMessagesResponseType>(
+        `/api/messages/${conversationId}/messages?limit=10${pageParam ? `&cursor=${pageParam}` : ""}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          signal,
+        },
+      ),
+    initialPageParam: "",
+    getNextPageParam: () => undefined,
+    getPreviousPageParam: (firstPage) => firstPage.nextCursor,
+  });
+
+  useEffect(() => {
+    if (!observerRef.current) return;
+    const observer = new IntersectionObserver((entries) => {
+      if (
+        entries[0].isIntersecting &&
+        hasPreviousPage &&
+        !isFetchingPreviousPage
+      ) {
+        fetchPreviousPage();
+      }
+    });
+    observer.observe(observerRef.current);
+    return () => observer.disconnect();
+  }, [hasPreviousPage, fetchPreviousPage, isFetchingPreviousPage]);
 
   const handleSubmit = (event: SubmitEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -60,8 +80,24 @@ export default function ActiveConversation() {
     setMessage("");
   };
 
+  if (isLoading) <LoadingSpinner />;
+  if (isError) return <ErrorBlock>{error.message}</ErrorBlock>;
   return (
     <section className="max-w-3xl mx-auto flex flex-col gap-4 p-2 md:p-4 bg-light">
+      <div className="flex items-center gap-2 overflow-y-auto">
+        <div ref={observerRef}></div>
+        <ul>
+          {data?.pages.map((page) =>
+            page.messages.map((msg) => (
+              <MessageItem
+                messageData={msg}
+                key={msg.id}
+                isSender={msg.sender === userId}
+              />
+            )),
+          )}
+        </ul>
+      </div>
       <form onSubmit={handleSubmit} className="flex flex-col gap-2">
         <TextAreaElement
           className="w-full"
