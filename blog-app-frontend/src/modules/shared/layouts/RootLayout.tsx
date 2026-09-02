3b42fa96d6d521b11cc84d81fdcdb7772203e1d8
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-09-02 11:09:54 +0200

    Fix padding in RootLayout

--- blog-app-frontend/src/modules/shared/layouts/RootLayout.tsx
+++ blog-app-frontend/src/modules/shared/layouts/RootLayout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout() {
     <ToastContextProvider>
       <WsContextProvider>
         <div className="min-h-screen flex flex-col gap-1">
-          <header className="sticky z-20 w-full mb-1 md:mb-2 lg:mb-3 flex justify-end bg-bg-header shadow-sm">
+          <header className="sticky z-20 p-1 w-full mb-1 md:mb-2 lg:mb-3 flex justify-end bg-bg-header shadow-sm md:p-0">
             <Navigation />
           </header>
           <main className="flex-1 p-2">
